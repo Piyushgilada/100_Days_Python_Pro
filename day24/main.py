f2c29269ead1bd430e473e68043e265e0a6ebc98
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-02-02 13:12:47 +0530

    Add files via upload

--- day24/main.py
+++ day24/main.py
@@ -1,1 +1,8 @@
+# file=open("my_file.txt")
+# print(file.read())
+# file.close()
 
+# # alternative to above
+# mode append=a,write=w,read=r
+with open("my_file.txt",mode='w') as file:
+    file.write(" Piyush")
