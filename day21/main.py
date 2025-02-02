997f57ade5757e16fb3118a05d44c86a95acb48c
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-02-02 13:10:48 +0530

    Add files via upload

--- day21/main.py
+++ day21/main.py
@@ -1,1 +1,21 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes=2
 
+    def breathe(self):
+        print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+    def swim(self):
+        print("moving in water")
+
+    def breathe(self):
+        super().breathe()
+        print("doing underwater")
+
+fish=Fish()
+fish.breathe()
+fish.swim()
+print(fish.num_eyes)
