9fc88b658f4a08cd9902a7249557c800741c804b
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-02-02 13:20:33 +0530

    Add files via upload

--- day30/main.py
+++ day30/main.py
@@ -1,1 +1,18 @@
+# try:
+#     file=open('text.txt')           # file not exist error
+#     dic1={'key':'value'}            # value error
+#     print(dic1['keyy'])
+# except FileNotFoundError:
+#     file=open('text.txt','w')
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"the key is {error_message} not exist")
+# finally:  # // jayega isme toh
+#     print("finally block")
 
+height=float(input("Enter Height "))
+weight=int(input("Enter Weight "))
+if height >3:
+    raise ValueError('human value can not be over 3 meter')
+bmi=weight / height**2
+print(bmi)
