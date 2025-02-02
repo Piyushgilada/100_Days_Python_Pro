974eed9c7f1659bf808c233645b25d1ed20c743f
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-02-02 13:03:56 +0530

    Add files via upload

--- Day17/quiz_game/main.py
+++ Day17/quiz_game/main.py
@@ -1,1 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
+# Create a question bank from data
+question_bank = []
+for item in question_data:
+    ques = item["text"]
+    answer = item["answer"]
+    new_question = Question(ques, answer)  # Use the Question class
+    question_bank.append(new_question)
+
+# Initialize the QuizBrain object
+quiz = QuizBrain(question_bank)
+
+# Run the quiz
+while quiz.still_has_question():
+    quiz.next_question()
