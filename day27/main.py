662b88b68fd40103d34c429c95287b284819ca38
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-02-02 13:16:55 +0530

    Add files via upload

--- day27/main.py
+++ day27/main.py
@@ -1,1 +1,43 @@
+from tkinter import *
 
+window=Tk()
+window.title('My First GUI Program')
+window.minsize(width=500,height=300)
+window.config(padx=100,pady=100)
+
+# Label
+my_label=Label(text="I am A Label",font=('arial',24,'italic'))
+my_label['text']='new text'
+my_label.config(text='new text')
+my_label.pack(expand=True)
+
+# my_label.place(x=100,y=100)
+# my_label.pack()
+
+# Button
+def button_clicked():
+    my_label['text']=input.get()
+button=Button(text='click me',command=button_clicked)
+button.pack(expand=True)
+
+
+# Entry
+input=Entry(width='10')
+print(input.get())
+input.pack(expand=True)
+# input.grid(column=1,row=1)
+
+# Text
+text=Text(height=5, width=30)
+text.focus()
+text.insert(END, "Example of entry text ...")
+text.pack(expand=True)
+
+
+# spin box
+# Scale
+# CheckBox
+# Radio Button
+# List Box
+
+window.mainloop()
---
+++ day27/practice.py
@@ -0,0 +1,17 @@
+# @ multiple argument
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum+=n
+#     return sum
+#
+# print(add(1,2,3,4,5,9,8,7,6,5))
+
+# @ keyword argument
+# def calculate(n,**kwargs):
+#     # print(kwargs)
+#     n+=kwargs['add']
+#     n*=kwargs['multiply']
+#     print(n)
+#
+# calculate(2,add=3,multiply=5)
